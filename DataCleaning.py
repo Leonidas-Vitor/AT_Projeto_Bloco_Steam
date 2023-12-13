fb2c38fb53c67ea24f1012485a7bdf399ee3ff29
Leonidas <leonidas.vitor@outlook.com>
2023-12-13 16:50:40 -0300

    :construction: Refinando as páginas já existentes 0.71

--- DataCleaning.py
+++ DataCleaning.py
@@ -92,11 +92,6 @@ with st.expander('Apps com tags indevidas'):
     st.dataframe(df_steam[df_steam['ContainForbiddenTag']==True][['name','id','tags']],hide_index=True,height=250,use_container_width=True)
 
 df_steam = df_steam[df_steam['ContainForbiddenTag']==False]
-st.subheader('Remoção de playtests',divider=True)
-
-df_steam['isPlaytest'] = df_steam['name'].apply(lambda n: 'Playtest' in n if n != None else False)
-
-st.dataframe(df_steam[df_steam['isPlaytest'] == True],hide_index=True,height=250,use_container_width=True)
 st.subheader('Preenchendo as durações ausentes',divider=True)
 
 st.markdown(at_lib.GetBasicTextMarkdown(20,
