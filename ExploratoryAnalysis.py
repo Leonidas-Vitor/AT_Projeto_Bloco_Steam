ffa4d6018d3ed6df49e5eb3bbb2b7abcfd618c90
Leonidas <leonidas.vitor@outlook.com>
2023-12-13 19:15:47 -0300

    :construction: Refinando as páginas já existentes 0.93

--- ExploratoryAnalysis.py
+++ ExploratoryAnalysis.py
@@ -44,8 +44,9 @@ for r in range(x_plots):
         colName = df_steam_numerics.columns[i]
         if colName == 'total_reviews':
             i += 1
+            colName = df_steam_numerics.columns[i]
         sb.boxplot(data=df_steam_numerics[colName],  ax=axs[r, c], orient='h',color=sb.color_palette()[i % len(sb.color_palette())])
-        i+= 1
+        i += 1
         
 plt.subplots_adjust(wspace=0.3, hspace=0.3)
 st.pyplot(fig)
